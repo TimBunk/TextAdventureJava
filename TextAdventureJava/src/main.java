43ece305917680c891c4e975f5272488c9aa68f1
TimBunk <timbunk12@kpnmail.nl>
2019-12-16 21:55:17 +0100

    main.java bijgewerkt

--- TextAdventureJava/src/main.java
+++ TextAdventureJava/src/main.java
@@ -1,26 +1,38 @@
+import org.joml.Vector2f;
 import org.joml.Vector3f;
+
+import java.io.IOException;
 
 public class main {
 
-    public static void main(String args[]) {
-
-        System.out.println("test");
+    public static void main(String args[]) throws IOException {
 
         Window window = new Window(960, 540, "TextAdventureJava");
-
         window.use();
         window.setVSync(true);
         window.setBackgroundColor(new Vector3f(0.0f, 0.0f, 1.0f));
 
+        TextureManager textureManager = new TextureManager();
+
+        Sprite sprite = new Sprite(250, 250, textureManager.load("Images/awesomeface.png"));
+        sprite.setPosition(new Vector2f(480, 270));
+        Sprite sprite2 = new Sprite(250, 250, textureManager.load("Images/awesomeface.png"));
+        sprite2.setPosition(new Vector2f(0, 270));
+
+        Renderer renderer = new Renderer();
+        renderer.addSprite(sprite);
+        renderer.addSprite(sprite2);
+
         while (!window.shouldClose()) {
-            window.clear();
 
             // Rendering...
+            renderer.render(window);
 
-            window.swapBuffers();
             Window.pollEvents();
         }
 
+        textureManager.destroy();
+        renderer.destroy();
         window.destroy();
 
     }

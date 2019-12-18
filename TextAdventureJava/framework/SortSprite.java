5ecd6d9093042c73f4a857749d6997284c497b7c
TimBunk <timbunk12@kpnmail.nl>
2019-12-18 14:14:22 +0100

    Texture is nu een textureID geworden, vertices worden niet meer opgeslagen, de color bevat nu ook een alpha value, en er is een layer en id bijgekomen voor het sorteren van de sprites

---
+++ TextAdventureJava/framework/SortSprite.java
@@ -0,0 +1,19 @@
+import java.util.Comparator;
+
+/**
+ * @author Tim Bunk
+ * In deze SortSprite class staat een compare functie voor het sorteren van sprites.
+ * Sprites worden gesorteerd op layers, dan textures, dan id's
+ */
+public class SortSprite implements Comparator<Sprite> {
+
+    // https://www.geeksforgeeks.org/collections-sort-java-examples/
+    public int compare(Sprite sprite1, Sprite sprite2) {
+        // Layer > texture > id
+        int layerDiff = sprite1.getLayer() - sprite2.getLayer();
+        if (layerDiff != 0) { return layerDiff; }
+        int textureIDDiff = sprite1.getTextureID() - sprite2.getTextureID();
+        if (textureIDDiff != 0) { return textureIDDiff; }
+        return sprite1.getID() - sprite2.getID();
+    }
+}
--- TextAdventureJava/framework/Sprite.java
+++ TextAdventureJava/framework/Sprite.java
@@ -1,18 +1,23 @@
 import org.joml.Vector2f;
-import org.joml.Vector3f;
+import org.joml.Vector4f;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Tim Bunk
  * Dit is de sprite class in deze class staat alle informatie over een sprite
  */
 public class Sprite {
+    private static int counter = 0;
     private Vector2f pivot;
     private Vector2f position;
     private float rotation;
     private int width, height;
-    private Texture texture;
-    private Vector3f color;
-    private float[] vertices;
+    private int textureID;
+    private Vector4f color;
+    private int layer;
+    private int id;
 
     /**
      * Constructor van de sprite waar alles geinitialiseerd wordt
@@ -20,73 +25,64 @@ public class Sprite {
      * @param height de hoogte van de sprite
      * @param color de kleur van de sprite
      */
-    Sprite(int width, int height, Vector3f color) {
+    Sprite(int width, int height, Vector4f color) {
         this.width = width;
         this.height = height;
         this.color = color;
-        this.texture = null;
+        this.textureID = 0;
         pivot = new Vector2f(0.0f, 0.0f);
         position = new Vector2f(0.0f, 0.0f);
         rotation = 0.0f;
-        vertices = new float[16];
-        updateVertices();
+        layer = 0;
+        id = counter;
+        counter++;
     }
 
     /**
      * Constructor van de sprite waar alles geinitialiseerd wordt
      * @param width de breedte van de sprite
      * @param height de hoogte van de sprite
-     * @param texture de texture die deze sprite moet gebruiken
+     * @param textureID de texture die deze sprite moet gebruiken
      */
-    Sprite(int width, int height, Texture texture) {
+    Sprite(int width, int height, int textureID) {
         this.width = width;
         this.height = height;
-        this.color = new Vector3f(0.0f, 0.0f, 0.0f);
-        this.texture = texture;
+        this.color = new Vector4f(0.0f, 0.0f, 0.0f, 1.0f);
+        this.textureID = textureID;
         pivot = new Vector2f(0.0f, 0.0f);
         position = new Vector2f(0.0f, 0.0f);
         rotation = 0.0f;
-        vertices = new float[16];
-        updateVertices();
+        layer = 0;
+        id = counter;
+        counter++;
     }
 
     // Getters
-    public Vector3f getColor()      { return color; }
-    public Texture getTexture()     { return texture; }
+    public Vector4f getColor()      { return color; }
+    public int getTextureID()       { return textureID; }
     public float getRotation()      { return rotation; }
+    public int getLayer()           { return layer; }
+    public int getID()              { return id; }
     public int getWidth()           { return width; }
     public int getHeight()          { return height; }
     public Vector2f getPosition()   { return position; }
     public Vector2f getPivot()      { return pivot; }
-    public float[] getVertices()    { return vertices; }
 
     // Setters
-    public void setColor(Vector3f color)        { this.color = color; }
-    public void setTexture(Texture texture)   { this.texture = texture; }
+    public void setColor(Vector4f color)        { this.color = color; }
+    public void setTextureID(int textureID)     { this.textureID = textureID; }
     public void setRotation(float rotation)     { this.rotation = rotation; }
+    public void setLayer(int layer)             { this.layer = layer; }
     public void setWidth(int width) {
         this.width = width;
-        updateVertices();
     }
     public void setHeight(int height) {
         this.height = height;
-        updateVertices();
     }
     public void setPosition(Vector2f position) {
         this.position = position;
-        updateVertices();
     }
     public void setPivot(Vector2f pivot) {
         this.pivot = pivot;
-        updateVertices();
-    }
-
-
-    private void updateVertices() {
-        // Positions                                                                                                            // texture coords
-        vertices[0] = (width*(0.5f-(pivot.x*0.5f)) + position.x); vertices[1] = (height*(0.5f-(pivot.y*0.5f)) + position.y);    vertices[2] = 1.0f; vertices[3] = 1.0f;  // top right
-        vertices[4] = (width*(0.5f-(pivot.x*0.5f)) + position.x); vertices[5] = (-height*(0.5f-(pivot.y*0.5f)) + position.y);   vertices[6] =1.0f; vertices[7] = 0.0f;  // bottom right
-        vertices[8] =(-width*(0.5f-(pivot.x*0.5f)) + position.x); vertices[9] = (-height*(0.5f-(pivot.y*0.5f)) + position.y);   vertices[10] =0.0f; vertices[11] =0.0f;  // bottom left
-        vertices[12] =(-width*(0.5f-(pivot.x*0.5f)) + position.x); vertices[13] =(height*(0.5f-(pivot.y*0.5f)) + position.y);   vertices[14] =0.0f; vertices[15] =1.0f; // top left
     }
 }

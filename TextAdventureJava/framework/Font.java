07ba29b817968787a0622e9a947756810dadf820
TimBunk <timbunk12@kpnmail.nl>
2020-01-02 14:25:13 +0100

    De UV coordinates voor de fonts zijn nu genormaliseerd

--- TextAdventureJava/framework/Font.java
+++ TextAdventureJava/framework/Font.java
@@ -22,23 +22,24 @@ public class Font {
         filePath = getClass().getResource(String.format("%s%s", filePath, ".fnt")).getPath();
         BufferedReader br = new BufferedReader(new FileReader(filePath));
         String currentLine = "";
-        int scaleH = 0;
+        float scaleW = 0, scaleH = 0;
         while((currentLine = br.readLine()) != null) {
             String[] array = currentLine.split("=|\\s+");
             if (array.length != 0) {
                 if (array[0].equals("char")) {
-                    loadGlyphFromString(array, scaleH);
+                    loadGlyphFromString(array, scaleW, scaleH);
                 }
                 else if (array[0].equals("common")) {
-                    scaleH = Integer.parseInt(array[8]);
+                    scaleW = Float.parseFloat(array[6]);
+                    scaleH = Float.parseFloat(array[8]);
                 }
             }
         }
     }
 
-    private void loadGlyphFromString(String[] array, int scaleH) {
+    private void loadGlyphFromString(String[] array, float scaleW, float scaleH) {
         char id = (char)Integer.parseInt(array[2]);
-        Vector2f uv = new Vector2f(Integer.parseInt(array[4]), scaleH - Integer.parseInt(array[6]));
+        Vector2f uv = new Vector2f(Float.parseFloat(array[4])/scaleW, (scaleH - Float.parseFloat(array[6]))/scaleH);
         Vector2f scale = new Vector2f(Integer.parseInt(array[8]), Integer.parseInt(array[10]));
         Vector2f offset = new Vector2f(Integer.parseInt(array[12]), Integer.parseInt(array[14]));
         int xAdvance = Integer.parseInt(array[16]);

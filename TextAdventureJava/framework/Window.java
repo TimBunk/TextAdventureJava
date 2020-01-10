948e0c8f88e55dfde625ec608d7ae8ebeeb6fe84
TimBunk <timbunk12@kpnmail.nl>
2020-01-10 13:32:06 +0100

    2 hints toegevoegd aan de window class dat er voor zou moeten dat de framework ook op apple werkt

--- TextAdventureJava/framework/Window.java
+++ TextAdventureJava/framework/Window.java
@@ -57,6 +57,8 @@ public class Window {
         // Gebruik versie 3.3 van openGL
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
         // De window kan niet van grootte worden verandert
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // the window will be resizable
         // Maak de window
